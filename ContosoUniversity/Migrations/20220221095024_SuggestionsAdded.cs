a0e465666a4ff891b693c6b5385556594c2bc9fc
Ikponwosa <ikponwosae@gmail.com>
2022-02-21 11:59:10 +0100

    initial commit

---
+++ ContosoUniversity/Migrations/20220221095024_SuggestionsAdded.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class SuggestionsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suggestions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EnterDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Suggestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentID = table.Column<int>(type: "int", nullable: true),
+                    InstructorID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suggestions", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Suggestions_Instructor_InstructorID",
+                        column: x => x.InstructorID,
+                        principalTable: "Instructor",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Suggestions_Student_StudentID",
+                        column: x => x.StudentID,
+                        principalTable: "Student",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suggestions_InstructorID",
+                table: "Suggestions",
+                column: "InstructorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suggestions_StudentID",
+                table: "Suggestions",
+                column: "StudentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suggestions");
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Suggestions.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Suggestions
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [Display(Name = "Today's Date")]
+        public DateTime EnterDate { get; set; }
+
+        [Required]
+        public string Suggestion { get; set; }
+
+
+        public Student Student { get; set; }
+        public Instructor Instructor { get; set; }
+    }
+}
